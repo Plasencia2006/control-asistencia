fc6daf2ea2efef057aa1b67830ac1474d70ac622
Plasencia2006 <plasenciaalvahectorisai@gmail.com>
2026-05-06 10:31:05 -0500

    Fix Cloudinary configuration

--- config/settings.py
+++ config/settings.py
@@ -21,13 +21,28 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Apps locales
     'asistencia',
+    # Agrega esto:
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 # django-extensions solo para desarrollo
-if DEBUG:
-    INSTALLED_APPS += ['django_extensions']
+# Media files
+if not DEBUG:
+    # Cloudinary para producción
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+        'API_KEY': config('CLOUDINARY_API_KEY'),
+        'API_SECRET': config('CLOUDINARY_API_SECRET'),
+        'SECURE': True,
+    }
+    MEDIA_URL = '/media/'
+else:
+    # Local para desarrollo
+    MEDIA_URL = 'media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -145,3 +160,13 @@ if not DEBUG:
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
     X_FRAME_OPTIONS = 'DENY'
+    
+# Configuración explícita de Cloudinary
+if not DEBUG:
+    import cloudinary
+    cloudinary.config(
+        cloud_name=config('CLOUDINARY_CLOUD_NAME'),
+        api_key=config('CLOUDINARY_API_KEY'),
+        api_secret=config('CLOUDINARY_API_SECRET'),
+        secure=True
+    )
